1bd6558e3eccc14d5b3cdb22fa154426e4bb6bd2
Ben Forrester <bforest@redhat.com>
2026-09-15 22:15:12 -0400

    fixed ssh keys

--- terraform/main.tf
+++ terraform/main.tf
@@ -240,6 +240,7 @@ action "aap_job_launch" "configure_weather_app" {
       "web_node_ip"                     : aws_eip.web_eip.public_ip,
       "web_node_dns"                    : aws_eip.web_eip.public_dns,
       "db_node_ip"                      : aws_instance.db_tier.public_ip,
+      "db_private_ip"                   : aws_instance.db_tier.private_ip,
       "ansible_ssh_private_key_content" : "${trimspace(tls_private_key.demo_key.private_key_pem)}\n",
       "ansible_user"                    : "ec2-user",
       "weather_api_key"                 : var.weather_api_key,
